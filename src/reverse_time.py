07d021e7298a9d49fbb749aca5805a1bcdbbb713
ScottHull <scottdhull21@gmail.com>
2021-06-14 16:16:36 -0400

    testing elemeents

--- src/reverse_time.py
+++ src/reverse_time.py
@@ -139,7 +139,7 @@ class ReverseTime:
             linewidth=2.0,
             color='black',
             label="Angle: {}".format(
-                round(tan((self.com_impactor[1] - self.com_target[1]) / (self.com_impactor[1] - self.com_target[1])) * (
+                round(tan((self.com_impactor[1] - self.com_target[1]) / (self.com_impactor[0] - self.com_target[0])) * (
                             180 / pi), 4)
             )
         )
