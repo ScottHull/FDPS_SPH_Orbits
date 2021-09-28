8fa391f17fea340ed5fc0e0bff8e42710a4b5f5e
ScottHull <scottdhull21@gmail.com>
2021-09-28 11:50:19 -0400

    added track_high_entropy_particles.py

--- thermal_cross_section.py
+++ thermal_cross_section.py
@@ -7,9 +7,7 @@ from src.identify import ParticleMap
 from src.combine import CombineFile
 from src.animate import animate
 
-start_time = 0
-end_time = 3000
-interval = 20
+time = 3000
 number_processes = 100
 path = "/scratch/shull4/gi"
 output = "/scratch/shull4/thermal_map"
@@ -18,19 +16,19 @@ if os.path.exists(output):
     shutil.rmtree(output)
 os.mkdir(output)
 
-cf_end = CombineFile(num_processes=number_processes, time=end_time, output_path=path)
+cf_end = CombineFile(num_processes=number_processes, time=time, output_path=path)
 formatted_time_end = cf_end.sim_time
 combined_file_end = cf_end.combine()
-f = os.getcwd() + "/merged_{}.dat".format(end_time)
+f = os.getcwd() + "/merged_{}.dat".format(time)
 pm_end = ParticleMap(path=f, center=False, relative_velocity=False)
 particles = pm_end.collect_particles()
 pm_end.solve(particles=particles)
 os.remove(f)
 
-iron_particles = [i for i in particles if p.tag % 2 != 0]
-silicate_particles = [i for i in particles if p.tag % 2 == 0]
+iron_particles = [p for p in particles if p.tag % 2 != 0]
+silicate_particles = [p for p in particles if p.tag % 2 == 0]
 
-_min, _max = np.amin(np.array([i for i in particles.entropy])), np.amax([i for i in particles.entropy])
+_min, _max = np.amin(np.array([i.entropy for i in particles])), np.amax([i.entropy for i in particles])
 norm = plt.Normalize(_min, _max)
 fig = plt.figure(figsize=(16, 9))
 ax = fig.add_subplot(111)
