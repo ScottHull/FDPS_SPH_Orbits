b0e5a564ca631688d80549625c7610a7e97f2c1c
ScottHull <scottdhull21@gmail.com>
2021-10-14 10:34:07 -0400

    added vaporization.py

--- build_animation_from_existing.py
+++ build_animation_from_existing.py
@@ -3,7 +3,7 @@ from src.animate import animate
 start = 0
 end = 3000
 interval = 5
-fps = 5
+fps = 10
 from_path = "/home/theia/scotthull/FDPS_SPH_Orbits/new_and_old_animate"
 fname = "animate.mp4"
 
