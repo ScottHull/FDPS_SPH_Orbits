546b58f03744d05fa6062eadf49ded21148e47f0
ScottHull <scottdhull21@gmail.com>
2021-11-23 00:34:27 -0500

    working on adding pre-profiled reporting support

--- src/new_and_old_eos.py
+++ src/new_and_old_eos.py
@@ -56,6 +56,7 @@ def plot(fig, axs, particles, index, time, cmap, normalizer, square_scale, param
     ax.scatter(
         [p.position[0] for p in particles if p.position[2] < 0],
         [p.position[1] for p in particles if p.position[2] < 0],
+        c=[cmap(normalizer(get_parameter_from_particles(particle=p, parameter=parameter))) for p in particles if p.position[2] < 0],
         s=0.02,
         marker="o",
     )
