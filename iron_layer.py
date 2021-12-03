539a9e52f4ad70d37ceb5e1fb765d140c3648085
ScottHull <scottdhull21@gmail.com>
2021-12-03 14:54:40 -0500

    added iron_layer.py

--- iron_layer.py
+++ iron_layer.py
@@ -12,15 +12,22 @@ Volume of a spherical shell: 4/3 pi (r_2 - r_1)^3
 We need to calculate r_2 - r_1 (can't get it directly from simulation)
 Know that density is rho = M / V, so the density of the shell is rho = M / [4/3 pi (r_2 - r_1)^3]
 We can get M from the simulation, but what about density?
-One way:
+
+THERMAL EXPANSIVITY (alpha) : [1 / K]
+TEMPERATURE (T) : [K]
+DENSITY (rho) : [kg/m3]
+
+DELTA RHO = RHO_0 * ALPHA * DELTA T
+RHO_0_iron = 7500
+
 """
 
 time = 3000
+start_time = 0
 new_path = "/home/theia/scotthull/1M/formatted_gi_new_eos_b_073"
 old_path = "/home/theia/scotthull/1M/formatted_gi_old_eos_b_073"
 new_processes = 200
 old_processes = 200
-
 
 plt.style.use("dark_background")
 
@@ -31,18 +38,48 @@ labels = {
     3: "Impactor Iron"
 }
 
-def __mean_curve(x, y):
-    combined = list(zip(x, y))
-    sort = sorted(combined, key=lambda tup: tup[0])
-    x = [i[0] for i in sort]
-    y = [i[1] for i in sort]
-    s = UnivariateSpline(x, y)
-    xs = np.linspace(0, int(max(y)), 10000)
-    ys = s(xs)
-    return xs, ys
 
-def thermal_profile(new_particles, old_particles, earth_radius = 6371 * 1000):
+def map_t0_particles(t_0_particles):
+    mapped = {}
+    for p in t_0_particles:
+        mapped.update({p.particle_id: p})
+    return mapped
 
+def iron_layer_delta_temperature(t_particles, t_0_particles):
+    t_0_particles = map_t0_particles(t_0_particles=t_0_particles)
+    delta_t = [p.temperature - t_0_particles[p.particle_id].temperature for p in t_particles]
+    return delta_t
+
+
+
+def identify_iron_layer_particles(new_particles, old_particles, earth_radius=6371 * 1000):
+    new_impactor_iron_particles = [p for p in new_particles if p.label == "PLANET" and p.tag == 3]
+    old_impactor_iron_particles = [p for p in old_particles if p.label == "PLANET" and p.tag == 3]
+
+    fig, axs = plt.subplots(1, 2, figsize=(10, 16), sharex='all', sharey='all',
+                            gridspec_kw={"hspace": 0.0, "wspace": 0.10})
+    fig.patch.set_facecolor('xkcd:black')
+    new_ax, old_ax = axs.flatten()[0], axs.flatten()[1]
+    new_ax.scatter(
+        [p.distance / earth_radius for p in new_particles],
+        [p.temperature for p in new_particles],
+        s=2,
+        color='magenta'
+    )
+    old_ax.scatter(
+        [p.distance / earth_radius for p in new_impactor_iron_particles],
+        [p.temperature for p in old_impactor_iron_particles],
+        s=2,
+        color='magenta'
+    )
+    new_ax.set_ylabel("Temperature (K)")
+    for ax in [new_ax, old_ax]:
+        ax.set_xlabel(r'Radius $R_{\bigoplus}$')
+        ax.grid(alpha=0.4)
+    fig.save("iron_layer_particles.png", format='png')
+
+
+def thermal_profile(new_particles, old_particles, earth_radius=6371 * 1000):
     fig, axs = plt.subplots(1, 2, figsize=(10, 16), sharex='all', sharey='all',
                             gridspec_kw={"hspace": 0.0, "wspace": 0.10})
     fig.patch.set_facecolor('xkcd:black')
@@ -66,20 +103,6 @@ def thermal_profile(new_particles, old_particles, earth_radius = 6371 * 1000):
             label=labels[label],
             s=1
         )
-    new_mean_curve_x, new_mean_curve_y = __mean_curve(
-        x=[p.distance / earth_radius for p in new_particles if p.label == "PLANET"],
-        y=[p.temperature for p in new_particles if p.label == "PLANET"],
-    )
-    old_mean_curve_x, old_mean_curve_y = __mean_curve(
-        x=[p.distance / earth_radius for p in old_particles if p.label == "PLANET"],
-        y=[p.temperature for p in old_particles if p.label == "PLANET"],
-    )
-    new_ax.plot(
-        new_mean_curve_x, new_mean_curve_y, linewidth=2.0, color='white', label="Mean"
-    )
-    old_ax.plot(
-        old_mean_curve_x, old_mean_curve_y, lioldidth=2.0, color='white', label="Mean"
-    )
     legend = new_ax.legend()
     for handle in legend.legendHandles:
         try:
@@ -91,12 +114,45 @@ def thermal_profile(new_particles, old_particles, earth_radius = 6371 * 1000):
 
 
 new_particles, new_time = get_particles(path=new_path, number_processes=new_processes, time=time,
-                                                solve=False, formatted=True)
+                                        solve=False, formatted=True)
 old_particles, old_time = get_particles(path=old_path, number_processes=old_processes, time=time,
-                                                solve=False, formatted=True)
+                                        solve=False, formatted=True)
+new_particles_t0, new_time_t0 = get_particles(path=new_path, number_processes=new_processes, time=start_time,
+                                        solve=True, formatted=False)
+old_particles_t0, old_time_t0 = get_particles(path=old_path, number_processes=old_processes, time=start_time,
+                                        solve=True, formatted=False)
+
+new_t0_dict = map_t0_particles(t_0_particles=new_particles_t0)
+old_t0_dict = map_t0_particles(t_0_particles=old_particles_t0)
+new_delta_t = iron_layer_delta_temperature(t_0_particles=new_particles_t0, t_particles=new_particles)
+old_delta_t = iron_layer_delta_temperature(t_0_particles=old_particles_t0, t_particles=old_particles)
+fig, axs = plt.subplots(1, 2, figsize=(10, 16), sharex='all', sharey='all',
+                            gridspec_kw={"hspace": 0.0, "wspace": 0.10})
+fig.patch.set_facecolor('xkcd:black')
+new_ax, old_ax = axs.flatten()[0], axs.flatten()[1]
+new_ax.scatter(
+    [p.distance / (6371 * 1000) for p in new_particles],
+    new_delta_t,
+    color='magenta',
+    s=2
+)
+old_ax.scatter(
+    [p.distance / (6371 * 1000) for p in old_particles],
+    old_delta_t,
+    color='magenta',
+    s=2
+)
+new_ax.set_title("New EoS")
+old_ax.set_title("Old EoS")
+new_ax.set_ylabel("Delta T (K)")
+new_ax.set_ylabel("Temperature (K)")
+for ax in [new_ax, old_ax]:
+    ax.set_xlabel(r'Radius $R_{\bigoplus}$')
+    ax.grid(alpha=0.4)
+plt.savefig("delta_T_iron_layer.png", format='png')
+
+
+
+
 
 thermal_profile(new_particles=new_particles, old_particles=old_particles)
-
-
-
-
