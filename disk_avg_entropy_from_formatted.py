33584c66a85a5d6406599c0ed682ec9d0cadd20f
ScottHull <scottdhull21@gmail.com>
2021-12-07 11:41:46 -0500

    added disk_avg_entropy_from_formatted.py

--- disk_avg_entropy_from_formatted.py
+++ disk_avg_entropy_from_formatted.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
+import os
+import shutil
 import csv
 import pandas as pd
 import numpy as np
 from statistics import mean
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 new_path = "/home/theia/scotthull/1M/gi_new_eos_b_073_at_time"
 old_path = "/home/theia/scotthull/1M/gi_old_eos_b_073_at_time"
+path = "/home/theia/scotthull/FDPS_SPH_Orbits/disk_avgs"
 start_time = 0
-end_time = 2500
+end_time = 3000
 increment = 100
 
+if path in os.listdir(os.getcwd()):
+    shutil.rmtree(path)
+os.mkdir(path)
 
 def get_time(f):
     formatted_time = None
@@ -19,6 +26,18 @@ def get_time(f):
         formatted_time = float(next(reader)[0])
     infile.close()
     return formatted_time * 0.000277778  # seconds -> hours
+
+
+def get_particles_by_label(particles):
+    return {
+        "PLANET": [zip(x, particles['y'][index], particles['z'][index]) for index, x in enumerate(particles['x']) if
+                   particles['label'] == "PLANET"],
+        "DISK": [zip(x, particles['y'][index], particles['z'][index]) for index, x in enumerate(particles['x']) if
+                 particles['label'] == "DISK"],
+        "ESCAPE": [zip(x, particles['y'][index], particles['z'][index]) for index, x in enumerate(particles['x']) if
+                   particles['label'] == "ESCAPE"]
+    }
+
 
 new_times, old_times, new_avg_entropies, old_avg_entropies = [], [], [], []
 num_particles_disk_new, num_particles_disk_old = [], []
@@ -31,6 +50,10 @@ for time in np.arange(start_time, end_time + increment, increment):
     old_file = pd.read_csv(old_f, skiprows=2).to_dict()
     new_disk_entropies = [s for index, s in enumerate(new_file['entropy']) if new_file['label'][index] == "DISK"]
     old_disk_entropies = [s for index, s in enumerate(old_file['entropy']) if old_file['label'][index] == "DISK"]
+
+    labeled_particles_new = get_particles_by_label(new_file)
+    labeled_particles_old =  get_particles_by_label(old_file)
+
     if len(new_disk_entropies) > 0:
         new_avg_entropies.append(mean(new_disk_entropies))
     else:
@@ -43,29 +66,59 @@ for time in np.arange(start_time, end_time + increment, increment):
     num_particles_disk_new.append(len(new_disk_entropies))
     num_particles_disk_old.append(len(old_disk_entropies))
 
-fig = plt.figure(figsize=(16, 9))
-ax = fig.add_subplot(111)
-ax.plot(
-    new_times, new_avg_entropies, linewidth=2.0, label="New EoS"
-)
-ax.plot(
-    old_times, old_avg_entropies, linewidth=2.0, label="Old EoS"
-)
-ax.set_xlabel("Time (hrs)"), ax.set_ylabel("Avg. Disk Entropy")
-ax.grid(alpha=0.4)
-ax.legend()
-plt.savefig("avg_entropies_from_formatted.png", format='png')
+    gs = gridspec.GridSpec(2, 2)
+    fig = plt.figure(figsize=(10, 10))
+    fig.patch.set_facecolor('xkcd:black')
+    ax1 = fig.add_subplot(gs[0, 0])  # row 0, col 0
+    ax2 = fig.add_subplot(gs[0, 1])  # row 0, col 1
+    ax3 = fig.add_subplot(gs[1, 0])
+    ax4 = fig.add_subplot(gs[1, 1])
+    axs = [ax1, ax2, ax3, ax4]
+    for ax in axs:
+        ax.grid(alpha=0.4)
+    for label in labeled_particles_new.keys():
+        ax1.scatter(
+            [i[0] for i in labeled_particles_new[label] if i[2] < 0],
+            [i[1] for i in labeled_particles_new[label] if i[2] < 0],
+            s=1,
+            label=label
+        )
+    ax1.legend(loc='upper right')
+    for label in labeled_particles_old.keys():
+        ax2.scatter(
+            [i[0] for i in labeled_particles_old[label] if i[2] < 0],
+            [i[1] for i in labeled_particles_old[label] if i[2] < 0],
+            s=1,
+            label=label
+        )
+    ax2.legend(loc='upper right')
+    ax3.plot(
+        new_times, new_avg_entropies, linewidth=2.0, label="New EoS"
+    )
+    ax3.plot(
+        old_times, old_avg_entropies, lioldidth=2.0, label="Old EoS"
+    )
+    ax3.set_xlabel("Time (hrs)")
+    ax3.set_ylabel("Avg. Disk Entropy")
+    ax3.legend(loc='upper right')
+    ax4.plot(
+        new_times, num_particles_disk_new, linewidth=2.0, label="New EoS"
+    )
+    ax4.plot(
+        old_times, num_particles_disk_old, lioldidth=2.0, label="Old EoS"
+    )
+    ax4.set_xlabel("Time (hrs)")
+    ax4.set_ylabel("Num. Disk Particles")
+    ax4.legend(loc='upper right')
 
-fig = plt.figure(figsize=(16, 9))
-ax = fig.add_subplot(111)
-ax.plot(
-    new_times, num_particles_disk_new, linewidth=2.0, label="New EoS"
+    plt.savefig(path + "/{}.png".format(time), format='png')
+
+animate(
+    start_time=start_time,
+    end_time=end_time,
+    interval=increment,
+    path=path,
+    fps=10,
+    filename="disk_avg.mp4",
 )
-ax.plot(
-    old_times, num_particles_disk_old, linewidth=2.0, label="Old EoS"
-)
-ax.set_xlabel("Time (hrs)"), ax.set_ylabel("Num. Disk Particles")
-ax.grid(alpha=0.4)
-ax.legend()
-plt.savefig("num_disk_particles.png", format='png')
     
--- viscous_spreading.py
+++ viscous_spreading.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import pandas as pd
 import numpy as np
+from math import sqrt
 from statistics import mean
 import matplotlib.pyplot as plt
 
@@ -10,7 +11,7 @@ start_time = 0
 end_time = 2500
 increment = 100
 
-def calculate_smth_lengths(particles, dimension=3, eta=1.2):
+def calculate_smth_lengths(particle_density, particle_mass, dimension=3, eta=1.2):
     """
     Returns a list of smoothing lengths h.
     :param particles:
@@ -19,6 +20,25 @@ def calculate_smth_lengths(particles, dimension=3, eta=1.2):
     :return:
     """
     return [
-        eta * (mass / particles["density"][index]) ** (1 / dimension) for mass, index in particles['mass']
+        eta * (particle_mass / particle_density) ** (1 / dimension)
     ]
+
+def calculate_w_ij(particle_i_position, particle_j_position, particle_i_velocity, particle_j_velocity):
+    dr = [i - j for i, j in zip(particle_i_position, particle_j_position)]
+    dv = [i - j for i, j in zip(particle_i_velocity, particle_j_velocity)]
+    norm_dr = np.linalg.norm(dr)
+    norm_dv = np.linalg.norm(dv)
+    if norm_dr * norm_dv < 0:
+        return (norm_dr * norm_dv) / sqrt(norm_dr ** 2)
+    else:
+        return 0
+
+def calculate_v_sig(w_ij, particle_i_soundspeed, particle_j_soundspeed):
+    return (particle_i_soundspeed + particle_j_soundspeed) - (3 * w_ij)
+
+def calculate_vsig_max(curr_vsig, candidate_vsig):
+    if curr_vsig < candidate_vsig:
+        return candidate_vsig
+    return curr_vsig
+
 
