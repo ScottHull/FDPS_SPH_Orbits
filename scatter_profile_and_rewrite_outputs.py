0353f8c2bb79caa4a71c01b261ae681fb6df5929
ScottHull <scottdhull21@gmail.com>
2021-12-14 19:30:10 -0500

    working on iron_layer.py

--- scatter_profile_and_rewrite_outputs.py
+++ scatter_profile_and_rewrite_outputs.py
@@ -3,6 +3,7 @@ import numpy as np
 import csv
 import matplotlib.pyplot as plt
 from src.report import BuildReports
+import multiprocessing as mp
 
 """
 This code produces cumulative outputs, but only "solves" for the disk at certain time intervals.
@@ -12,36 +13,37 @@ Useful for getting a coarse understanding of disk mass change without having to 
 plt.style.use("dark_background")
 
 path = "/home/theia/scotthull/1M_high_rho_cutoff/gi_new_eos_b_073_high_rho_cutoff_1M"
+to_path = "/home/theia/scotthull/1M_high_rho_cutoff/formatted_gi_new_eos_b_073_high_rho_cutoff_1M"
+phase_curve = "/home/theia/scotthull/FDPS_SPH_Orbits/src/phase_data/forstSTS__vapour_curve.txt"
 num_processes = 200
 assess_at_interval = 100
 start_iteration = 0
 end_iteration = 2900
 end_disk_state_iteration = 1000
 
-def get_time(f):
-    formatted_time = None
-    with open(f, 'r') as infile:
-        reader = csv.reader(infile, delimiter="\t")
-        formatted_time = float(next(reader)[0])
-    infile.close()
-    return round(formatted_time * 0.000277778, 2)  # seconds -> hours
+os.mkdir(to_path)
 
-times = []
-for i in np.arange(0, 3000 + 1, 1):
-    fname = "results.{}_{}_{}.dat".format(str(i).zfill(5), str(num_processes).zfill(5), str(1).zfill(5))
-    f = path + "/{}.dat".format(fname)
-    time = get_time(f)
-    times.append(time)
-
-fig = plt.figure(figsize=(16, 9))
-ax = fig.add_subplot(111)
-ax.plot(
-    np.arange(0, 3000 + 1, 1),
-    times,
-    linewidth=2.0
+reports = BuildReports(
+    accessory_path="/home/theia/scotthull/1M_high_rho_cutoff/gi_new_eos_b_073_high_rho_cutoff_1M_disk_reports",
+    start_time=start_iteration,
+    end_time=end_disk_state_iteration,
+    eos_phase_path=phase_curve,
+    from_dir=path,
+    number_processes=num_processes,
+    to_dir=to_path
 )
-ax.axhline(30, color='red', linestyle="--")
-ax.set_xlabel("Iteration")
-ax.set_ylabel("Time (hrs)")
 
-plt.savefig("iteration_vs_time.png", format='png')
+def __loop(time):
+    print("At iteration {}".format(time))
+    if time % assess_at_interval:
+        reports.build_report_at_time(time=time, solve=True)
+    else:
+        reports.build_report_at_time(time=time, solve=False)
+
+pool = mp.Pool(5)
+for time in np.arange(0, end_iteration + 1, 1):
+    pool.map(__loop, [time])
+pool.close()
+pool.join()
+
+
