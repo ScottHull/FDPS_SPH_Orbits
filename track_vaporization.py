8c52edccee1669c4d4b25b9643fe78f02ee43f0b
ScottHull <scottdhull21@gmail.com>
2021-12-16 15:34:18 -0500

    added profile_target_and_impactor.py

--- track_vaporization.py
+++ track_vaporization.py
@@ -20,21 +20,23 @@ increment = 100
 def seconds_to_hours(seconds):
     return round(seconds * 0.000277778, 2)
 
-times, vmfs, mean_entropy = [], [], []
+times, vmfs, mean_entropy, num_disk_particles = [], [], [], []
 for time in np.arange(start_time, end_time + increment, increment):
     f = path + "/{}.csv".format(time)
 
     particles, formatted_time = get_particles(path=path, formatted=True, time=time, number_processes=200)
     formatted_time = seconds_to_hours(formatted_time)
     mean_disk_entropy = mean([p.entropy for p in particles if p.label == "DISK"])
+    number_disk_particles = len([p.entropy for p in particles if p.label == "DISK"])
 
     vmf = calc_vapor_mass_fraction(particles=particles, phase_path=phase_path, only_disk=True) * 100.0
     times.append(formatted_time)
     vmfs.append(vmf)
     mean_entropy.append(mean_disk_entropy)
+    num_disk_particles.append(number_disk_particles)
 
-fig, axs = plt.subplots(1, 3, figsize=(16, 20), sharex='all', gridspec_kw={"hspace": 0.10, "wspace": 0.14})
-ax1, ax2 = axs.flatten()
+fig, axs = plt.subplots(1, 2, figsize=(16, 9), sharex='all', gridspec_kw={"hspace": 0.10, "wspace": 0.14})
+ax1, ax2, ax3 = axs.flatten()
 ax1.plot(
     times,
     vmfs,
@@ -48,6 +50,13 @@ ax2.plot(
     linewidth=2.0
 )
 ax2.set_ylabel("Avg. Disk Entropy")
+
+ax3.plot(
+    times,
+    num_disk_particles,
+    linewidth=2.0
+)
+ax3.set_ylabel("Number Disk Particles")
 
 for ax in axs.flatten():
     ax.set_xlabel("Time (hrs)")
