47206c16320dbd123e215a7a5b9f0aa33d7acdf6
ScottHull <scottdhull21@gmail.com>
2021-12-29 12:44:05 -0500

    added vmf_from_formatted.py

--- vmf_from_formatted.py
+++ vmf_from_formatted.py
@@ -1,17 +1,25 @@
 import numpy as np
 import pandas as pd
+import csv
 from statistics import mean
 import matplotlib.pyplot as plt
 
 from src.time import get_max_time, seconds_to_hours
 from src.vapor import calc_vapor_mass_fraction_from_formatted
 
-
 path = "/home/theia/scotthull/1M_500kgm3_cutoff/formatted_gi_new_eos_b_073_500kgm3_end_2900"
 phase_path = "src/phase_data/forstSTS__vapour_curve.txt"
 start_time = 0
 end_time = 2900
 increment = 100
+
+def get_time(f):
+    formatted_time = None
+    with open(f, 'r') as infile:
+        reader = csv.reader(infile, delimiter="\t")
+        formatted_time = float(next(reader)[0])
+    infile.close()
+    return formatted_time * 0.000277778  # seconds -> hours
 
 plt.style.use("dark_background")
 max_time = seconds_to_hours(get_max_time(max_iteration=end_time, path=path))
@@ -21,7 +29,7 @@ disk_particle_count = []
 avg_disk_entropy = []
 for time in np.arange(start_time, end_time + increment, increment):
     f = path + "/{}.csv".format(time)
-    times.append(get_max_time(max_iteration=time, path=path))
+    times.append(get_time(f))
     df = pd.read_csv(f, skiprows=2)
     vmf = calc_vapor_mass_fraction_from_formatted(df=df, phase_path=phase_path) * 100.0
     vmfs.append(vmf)
@@ -39,13 +47,9 @@ for time in np.arange(start_time, end_time + increment, increment):
                                                                                   num_disk_particles)
     )
 
-
-
-fig, axs = plt.subplots(1, 3, figsize=(16, 9), sharex="all", gridspec_kw={"wspace": 0.14})
-axs = axs.flatten()
-ax1, ax2, ax3 = axs
-for ax in axs:
-    ax.set_xlim(0, max_time)
+fig, axs = plt.subplots(1, 3, figsize=(16, 9), sharex="all", gridspec_kw={"wspace": 0.4})
+ax1, ax2, ax3 = axs.flatten()
+for ax in axs.flatten():
     ax.grid(alpha=0.4)
     ax.set_xlabel("Time (hrs)")
 ax1.plot(
