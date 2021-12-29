ec48a2d418469f72442d5eb4cadd9206d8585880
ScottHull <scottdhull21@gmail.com>
2021-12-29 12:31:17 -0500

    added vmf_from_formatted.py

--- vmf_from_formatted.py
+++ vmf_from_formatted.py
@@ -7,7 +7,7 @@ from src.time import get_max_time, seconds_to_hours
 from src.vapor import calc_vapor_mass_fraction_from_formatted
 
 
-path = "/home/theia/scotthull/1M_500kgm3/formatted_gi_new_eos_b_073_500kgm3_end_2900"
+path = "/home/theia/scotthull/1M_500kgm3_cutoff/formatted_gi_new_eos_b_073_500kgm3_end_2900"
 phase_path = "src/phase_data/forstSTS__vapour_curve.txt"
 start_time = 0
 end_time = 2900
@@ -26,9 +26,18 @@ for time in np.arange(start_time, end_time + increment, increment):
     vmf = calc_vapor_mass_fraction_from_formatted(df=df, phase_path=phase_path) * 100.0
     vmfs.append(vmf)
 
-    disk_particles = df[df['label' == "DISK"]]
-    avg_disk_entropy.append(mean(disk_particles['entropy']))
-    disk_particle_count.append(len(disk_particles['entropy']))
+    disk_particles = df[df['label'] == "DISK"]
+    try:
+        avg_disk_entropy_at_time = mean(disk_particles['entropy'])
+    except:
+        avg_disk_entropy_at_time = 0
+    avg_disk_entropy.append(avg_disk_entropy_at_time)
+    num_disk_particles = len(disk_particles['entropy'])
+    disk_particle_count.append(num_disk_particles)
+    print(
+        "TIME: {}\nVMF: {}\nAVG DISK ENTROPY: {}\nDISK PARTICLE COUNT: {}".format(time, vmf, avg_disk_entropy_at_time,
+                                                                                  num_disk_particles)
+    )
 
 
 
@@ -49,4 +58,4 @@ ax3.plot(
     times, disk_particle_count, linewidth=2.0
 )
 ax1.set_ylabel("VMF (%)"), ax2.set_ylabel("Avg. Disk Entropy"), ax3.set_ylabel("# Disk Particles")
-plt.savefig("vmf_from_formatted.png", formatted='png')
+plt.savefig("vmf_from_formatted.png", format='png')
