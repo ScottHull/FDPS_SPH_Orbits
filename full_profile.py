c6f28592a18ad022908009ee4229001ded4b725f
ScottHull <scottdhull21@gmail.com>
2022-01-11 13:55:28 -0500

    added full_profile.py

--- full_profile.py
+++ full_profile.py
@@ -5,70 +5,70 @@ base_path = "/home/theia/scotthull/Paper1_SPH/gi/"
 gi_b73_runs = {
     "5_b073_new": {
         "name": "5_b073_new",
-        "path": base_path + "5_b073_new"
+        "path": base_path + "formatted_5_b073_new"
     },
     "5_b073_old": {
         "name": "5_b073_old",
-        "path": base_path + "5_b073_old"
+        "path": base_path + "formatted_5_b073_old"
     },
     "500_b073_new": {
         "name": "500_b073_new",
-        "path": base_path + "500_b073_new"
+        "path": base_path + "formatted_500_b073_new"
     },
     "500_b073_old": {
         "name": "500_b073_old",
-        "path": base_path + "500_b073_old"
+        "path": base_path + "formatted_500_b073_old"
     },
     "1000_b073_new": {
         "name": "1000_b073_new",
-        "path": base_path + "1000_b073_new"
+        "path": base_path + "formatted_1000_b073_new"
     },
     "1000_b073_old": {
         "name": "1000_b073_old",
-        "path": base_path + "1000_b073_old"
+        "path": base_path + "formatted_1000_b073_old"
     },
     "2000_b073_new": {
         "name": "2000_b073_new",
-        "path": base_path + "2000_b073_new"
+        "path": base_path + "formatted_2000_b073_new"
     },
     "2000_b073_old": {
         "name": "2000_b073_old",
-        "path": base_path + "2000_b073_old"
+        "path": base_path + "formatted_2000_b073_old"
     },
 }
 
 gi_b75_runs = {
     "5_b075_new": {
         "name": "5_b075_new",
-        "path": base_path + "5_b075_new"
+        "path": base_path + "formatted_5_b075_new"
     },
     "5_b075_old": {
         "name": "5_b075_old",
-        "path": base_path + "5_b075_old"
+        "path": base_path + "formatted_5_b075_old"
     },
     "500_b075_new": {
         "name": "500_b075_new",
-        "path": base_path + "500_b075_new"
+        "path": base_path + "formatted_500_b075_new"
     },
     "500_b075_old": {
         "name": "500_b075_old",
-        "path": base_path + "500_b075_old"
+        "path": base_path + "formatted_500_b075_old"
     },
     "1000_b075_new": {
         "name": "1000_b075_new",
-        "path": base_path + "1000_b075_new"
+        "path": base_path + "formatted_1000_b075_new"
     },
     "1000_b075_old": {
         "name": "1000_b075_old",
-        "path": base_path + "1000_b075_old"
+        "path": base_path + "formatted_1000_b075_old"
     },
     "2000_b075_new": {
         "name": "2000_b075_new",
-        "path": base_path + "2000_b075_new"
+        "path": base_path + "formatted_2000_b075_new"
     },
     "2000_b075_old": {
         "name": "2000_b075_old",
-        "path": base_path + "2000_b075_old"
+        "path": base_path + "formatted_2000_b075_old"
     },
 }
 
