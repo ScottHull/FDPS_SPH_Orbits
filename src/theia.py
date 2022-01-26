461f4d0ab531511e96a5461a3c7295e8d18378df
ScottHull <scottdhull21@gmail.com>
2022-01-26 10:24:19 -0500

    added full_profile.py

--- src/theia.py
+++ src/theia.py
@@ -6,8 +6,8 @@ from scp import SCPClient
 class LunaToTheia:
 
     def __init__(self):
-        self.theia_server = "epsl.earth.rochester.edu"
-        self.theia_user = "scotthull"
+        self.theia_server = "SERVER"
+        self.theia_user = "USERNAME"
         self.theia_pw = "PASSWORD"
         # self.theia_client = self.createSSHClient(self.theia_server, self.theia_user, self.theia_pw).open_sftp()
 
