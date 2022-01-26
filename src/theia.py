089f7b508e039e80f7f1cea50ff92a820d2835f9
ScottHull <scottdhull21@gmail.com>
2022-01-26 10:20:40 -0500

    added full_profile.py

--- src/theia.py
+++ src/theia.py
@@ -1,4 +1,26 @@
-
+#!/usr/bin/env python3
+import os
+import paramiko
+from scp import SCPClient
 
 class LunaToTheia:
-    pass
+
+    def __init__(self):
+        self.theia_server = "epsl.earth.rochester.edu"
+        self.theia_user = "scotthull"
+        self.theia_pw = "Sunshine9249"
+        # self.theia_client = self.createSSHClient(self.theia_server, self.theia_user, self.theia_pw).open_sftp()
+
+    def createSSHClient(self, server, user, password):
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(server, username=user, password=password, port=22)
+        return client
+
+    def get_file(self, client, path, fname):
+        f = path + "/" + fname
+        return client.open(f)
+
+
+
