6a955df87dbcd7625c4ea16a2d63dee37ce96c94
ScottHull <scottdhull21@gmail.com>
2022-02-18 14:44:41 -0500

    added full_profile.py

--- track_target_com.py
+++ track_target_com.py
@@ -14,15 +14,15 @@ from_path = "/home/theia/scotthull/Paper1_SPH/tar-imp/5_new_high/target_5_new_hi
 to_path = "animate_target"
 start_iteration = 0
 end_iteration = 60
-inc = 5
+inc = 2
 num_proc = 400
 square_scale = 1e5 * 1000
 
 def calc_center_of_mass(mass, x, y, z):
     total_mass = sum(mass)
-    com_x = (mass * x) / total_mass
-    com_y = (mass * y) / total_mass
-    com_z = (mass * z) / total_mass
+    com_x = sum(mass * x) / total_mass
+    com_y = sum(mass * y) / total_mass
+    com_z = sum(mass * z) / total_mass
     return com_x, com_y, com_z
 
 def get_time(f, local=True):
@@ -45,6 +45,7 @@ for iteration in np.arange(start_iteration, end_iteration + inc, inc):
     mass, x, y, z = df[2], df[3], df[4], df[5]
     com_x, com_y, com_z = calc_center_of_mass(mass, x, y, z)
     coms.append(np.linalg.norm(com_x, com_y, com_y))
+    os.remove(fname)
 
 fig = plt.figure(figsize=(16, 9))
 ax = fig.add_subplot(111)
