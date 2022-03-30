f003336bea9d154080a6cb8f7d76271653f7b429
Scott Hull <scottdhull21@gmail.com>
2022-03-30 15:46:11 -0400

    added plot_target_or_impactor.py

---
+++ accessory/fix_miki_formatted_files.py
@@ -0,0 +1,25 @@
+import os
+import csv
+import pandas as pd
+
+def get_time_and_particle_count(f):
+    formatted_time = None
+    particle_count = None
+    with open(f, 'r') as infile:
+        reader = csv.reader(infile, delimiter="\t")
+        formatted_time = float(next(reader)[0])
+        particle_count = int(next(reader)[0])
+    infile.close()
+    return formatted_time, particle_count
+
+to_path = "/home/theia/scotthull/Paper1_SPH/gi/5_b073_new_high/miki_combined_format"
+
+for file in os.listdir(to_path):
+    df = pd.read_csv(file, skiprows=2, delimiter=" ")
+    for header in df.keys():
+        if header not in ["ID", "TYPE"]:
+            df[header] = ["{:.8E}".format(i) for i in df['header']]
+    os.remove(to_path)
+    fname = str(int(file.replace(".csv", ""))).zfill(4)
+    df.to_csv(to_path + "/{}.dat".format(fname), sep=" ")
+
