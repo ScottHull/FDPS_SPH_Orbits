3ef4662340bdfd30ca95bc0c62df41037a4c203b
ScottHull <scottdhull21@gmail.com>
2022-04-05 12:25:24 -0400

    updates to report.py

--- src/report.py
+++ src/report.py
@@ -158,9 +158,13 @@ def build_latex_table_from_disk_report(run_names: list, run_titles: list, to_bas
         row = [rows['header']]
         for index, run in enumerate(run_names):
             table_header.append(run_titles[index])
-            path = to_base_path + "{}/{}_reports/"
-            df = pd.read_csv(path + "{}.csv".format(iteration))
-            row.append(df[header][0])
+            try:
+                path = to_base_path + "{}/{}_reports/"
+                df = pd.read_csv(path + "{}.csv".format(iteration))
+                row.append(str(df[header][0]))
+            except Exception as e:
+                print(e)
+                row.append("")
         run_rows.append(row)
     with open(filename, 'r') as outfile:
         header_line = ""
