0d7100942c63f2be2c27ad1191d66618deca7439
Scott Hull <scottdhull21@gmail.com>
2022-04-11 15:08:25 -0400

    modified report.py

--- fix_vmf_reports.py
+++ fix_vmf_reports.py
@@ -56,11 +56,15 @@ def reformat():
             vmf_uncirc = calc_vapor_mass_fraction_from_formatted(df=df_formatted, phase_path=phase_path)
             vmf_circ = calc_vapor_mass_fraction_with_circularization_from_formatted(df=df_formatted,
                                                                                     phase_path=phase_path)
-            del df_report['DISK VMF']
-            df_report['DISK_VMF_W_CIRC'] = [str(vmf_circ * 100.0) + " %"]
-            df_report['DISK_VMF_WITHOUT_CIRC'] = [str(vmf_uncirc * 100.0) + " %"]
-            to_f = report_path = base_path + "{}/{}_reports/".format(sim, sim) + "{}.csv".format(iteration)
-            df_report.to_csv(to_f, index=False)
-            print("Rewrote report at {}".format(to_f))
+            try:
+                del df_report['DISK VMF']
+                df_report['DISK_VMF_W_CIRC'] = [str(vmf_circ * 100.0) + " %"]
+                df_report['DISK_VMF_WITHOUT_CIRC'] = [str(vmf_uncirc * 100.0) + " %"]
+                to_f = report_path + "{}.csv".format(iteration)
+                df_report.to_csv(to_f, index=False)
+                print("Rewrote report at {}".format(to_f))
+            except Exception as e:
+                print(e)
+                pass
 
 reformat()
