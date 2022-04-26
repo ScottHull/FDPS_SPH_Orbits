489e766d1b6c0ec99ed109bf6ba91c035533b1b7
ScottHull <scottdhull21@gmail.com>
2022-04-26 10:58:53 -0400

    added animate_secondary_tail.py

--- animate_secondary_tail.py
+++ animate_secondary_tail.py
@@ -249,6 +249,7 @@ def plot_time(dfs, sis, tails, endstates, to_path, iteration, time):
         tail = tails[t][-1]
         tail_in_disk = curr_disk[curr_disk.index.isin(tail['id'].tolist())]
         tail_not_in_disk = tail[~tail['id'].isin(tail_in_disk.index.tolist())]
+        tail_not_in_disk = df[df['id'].isin(tail_not_in_disk.index.to_list())]
         # tail_not_in_disk = tail_not_in_disk[~tail_not_in_disk['id'].isin(tail_in_disk.index.tolist())]
         disk_rest = curr_disk[~curr_disk.index.isin(tail_in_disk.index.tolist())]
         disk_rest = disk_rest[~disk_rest.index.isin(tail_not_in_disk['id'].tolist())]
