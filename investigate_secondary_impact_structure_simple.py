e5c233f0a1eeaf71a76fd8892b0fe4417d9f7e3b
ScottHull <scottdhull21@gmail.com>
2022-05-02 15:29:22 -0400

    added animate_secondary_tail.py

--- investigate_secondary_impact_structure_simple.py
+++ investigate_secondary_impact_structure_simple.py
@@ -311,8 +311,8 @@ def profile_time():
         si_velocity_vec = zip(si['vx'], si['vy'], si['vz'])
         tail_velocity_vec = zip(tail['vx'], tail['vy'], tail['vz'])
 
-        angular_momentum_si = mass_si * sum(np.array([np.cross(i, j) for i, j in zip(si_radial_vec, si_velocity_vec)])) / L_EM
-        angular_momentum_tail = mass_tail * sum(np.array([np.cross(i, j) for i, j in zip(tail_radial_vec, tail_velocity_vec)])) / L_EM
+        angular_momentum_si = (mass_si * sum(np.array([np.cross(i, j) for i, j in zip(si_radial_vec, si_velocity_vec)]))) / L_EM
+        angular_momentum_tail = (mass_tail * sum(np.array([np.cross(i, j) for i, j in zip(tail_radial_vec, tail_velocity_vec)]))) / L_EM
 
         data[t].update({
             "MASS SI": mass_si,
