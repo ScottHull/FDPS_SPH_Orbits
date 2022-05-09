817fcbc42a821d47b11f11d5f08f51d5010d3199
ScottHull <scottdhull21@gmail.com>
2022-05-09 11:32:53 -0400

    added source_timeshots_single_eos.py

--- plot_secondary_impact_angles_correlations.py
+++ plot_secondary_impact_angles_correlations.py
@@ -238,7 +238,7 @@ def plot_vs_disk_property_all():
     x_label_r_dot_v = r"$\phi$"
     sims, titles = get_all_sims(high=False)
     points = ["DISK_MASS", "DISK_ANGULAR_MOMENTUM"]
-    fig, axs = plt.subplots(2, 3, figsize=(16, 9), gridspec_kw={"hspace": 0.24, "wspace": 0.20})
+    fig, axs = plt.subplots(1, 3, figsize=(18, 6), gridspec_kw={"hspace": 0.24, "wspace": 0.20})
     axs = axs.flatten()
     colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
     for s, t in zip(sims, titles):
@@ -321,6 +321,7 @@ def plot_vs_disk_property_all():
 
         for ax in axs:
             ax.set_xlabel(r"$\phi$ (degrees)")
+            ax.set_aspect('equal')
         axs[0].set_ylabel("Time of Secondary Impact (hrs)")
         axs[1].set_ylabel("Disk Mass ($M_{L}$)")
         axs[2].set_ylabel("Disk Angular Momentum ($L_{EM}$)")
