665ee3fe5229146aa210f4b43304abc52b04d7c3
ScottHull <scottdhull21@gmail.com>
2022-05-17 16:49:32 -0400

    Working ANEOS

--- profile_imp.py
+++ profile_imp.py
@@ -41,6 +41,25 @@ for i in ['new', 'old']:
         axs[3].plot(
             df['radius'], df['entropy'], linewidth=2.0, label=name
         )
+        if cd == 5 and i == "new":
+            name = "{}_{}_high".format(cd, i)
+            f = base_path + "{}/imp.dat".format(name)
+            headers = ["id", "tag", "mass", "x", "y", "z", "vx", "vy", "vz", "density", "internal energy", "pressure",
+                       "potential energy", "entropy", "temperature"]
+            df = pd.read_csv(f, skiprows=2, header=None, delimiter="\t", names=headers)
+            df['radius'] = [((i ** 2 + j ** 2 + k ** 2) ** 0.5) / 1000.0 for i, j, k in zip(df['x'], df['y'], df['z'])]
+            axs[0].plot(
+                df['radius'], df['density'], linewidth=2.0, label=name
+            )
+            axs[1].plot(
+                df['radius'], df['pressure'], linewidth=2.0, label=name
+            )
+            axs[2].plot(
+                df['radius'], df['internal energy'], linewidth=2.0, label=name
+            )
+            axs[3].plot(
+                df['radius'], df['entropy'], linewidth=2.0, label=name
+            )
 
     for ax in axs:
         ax.grid(alpha=0.4)
@@ -51,4 +70,5 @@ for i in ['new', 'old']:
     axs[2].set_ylabel("Internal Energy (J)")
     axs[3].set_ylabel("Entropy (J/kg/K)")
     fig.suptitle(eos)
+    axs[0].legend(loc='upper left')
     plt.savefig("{}_impactor_profile.png".format(eos), format='png', dpi=200)
