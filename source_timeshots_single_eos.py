61392649afcf105014d970a418029c259fb64530
ScottHull <scottdhull21@gmail.com>
2022-07-31 00:43:12 -0400

    tweaks to source_timeshots_single_eos.py

--- source_timeshots_single_eos.py
+++ source_timeshots_single_eos.py
@@ -172,7 +172,7 @@ for index, ax in enumerate(axs):
     y_loc = y2 - (0.08 * (y2 - y1))
     ax.text(x_loc, y_loc, letters[index], fontweight="bold")
 
-axs[0].annotate("x ($10^4$ km)", xy=(0.0, -5.5), fontsize=16)
-axs[0].annotate("y ($10^4$ km)", xy=(-5.5, 0.0), rotation=90, fontsize=16)
+axs[0].annotate("x ($10^4$ km)", xy=(0.0, -5.5), ha="center", fontsize=16)
+axs[0].annotate("y ($10^4$ km)", xy=(-5.5, 0.0), va="center", rotation=90, fontsize=16)
 
 plt.savefig("source_scenes_{}_{}.png".format(angle, runs), format='png', dpi=200)
