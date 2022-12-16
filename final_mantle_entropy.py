b3fd6396db9401df1fef0a516779d74998fe1c0f
Scott Hull <ScottHull@users.noreply.github.com>
2022-12-16 15:52:42 -0500

    added velocity_profile_final_disk_particles.py

--- final_mantle_entropy.py
+++ final_mantle_entropy.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 plt.style.use('seaborn-colorblind')
 
 base_path = "/home/theia/scotthull/Paper1_SPH/gi/"
+initial_iteration = 0
 end_iteration = 1800
 # read in all the subdirectory names as the run names
 runs = [name for name in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, name))]
@@ -16,11 +17,18 @@ axs = axs.flatten()
 
 for run in runs:
     circ_path = base_path + f"/{run}/circularized_{run}"
+    start_time_df = pd.read_csv(circ_path + f"/{initial_iteration}.csv")
     end_time_df = pd.read_csv(circ_path + f"/{end_iteration}.csv")
     planet = end_time_df[end_time_df["label"] == "PLANET"]
     # get all the particles within 1e7 of the planet
     filtered_planet = end_time_df[end_time_df['radius'] < 1e7]
     filtered_planet = filtered_planet[filtered_planet['tag'] % 2 == 0]  # only silicate particles
+    corresponding_start_time = start_time_df[start_time_df["id"].isin(filtered_planet["id"].values)]
+    # calculate the fraction of particles where the entropy has increased from the start time to end time by at least 500
+    entropy_increase = filtered_planet["entropy"] - corresponding_start_time["entropy"]
+    entropy_increase = entropy_increase[entropy_increase > 500]
+    entropy_increase_fraction = entropy_increase.count() / filtered_planet["entropy"].count()
+    print(f"{run}: {entropy_increase_fraction}")
     plotting_index = 0
     if "new" in run and "b073" in run and "5" in run and 'high' not in run and "500" not in run:
         plotting_index = 0
