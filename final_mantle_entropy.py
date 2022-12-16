9d259ec134ca52e688dd512acb90daea7a9338b8
Scott Hull <ScottHull@users.noreply.github.com>
2022-12-16 14:44:01 -0500

    added velocity_profile_final_disk_particles.py

--- final_mantle_entropy.py
+++ final_mantle_entropy.py
@@ -11,7 +11,7 @@ end_iteration = 1800
 # read in all the subdirectory names as the run names
 runs = [name for name in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, name))]
 
-fig, axs = plt.subplots(2, 2, figsize=(10, 10), sharex="all", sharey="all")
+fig, axs = plt.subplots(2, 4, figsize=(10, 20), sharex="all", sharey="all")
 axs = axs.flatten()
 
 for run in runs:
@@ -22,24 +22,36 @@ for run in runs:
     filtered_planet = end_time_df[end_time_df['radius'] < 1e7]
     filtered_planet = filtered_planet[filtered_planet['tag'] % 2 == 0]  # only silicate particles
     plotting_index = 0
-    if "new" in run and "b073" in run:
+    if "new" in run and "b073" in run and "5" in run:
         plotting_index = 0
-    elif "old" in run and "b073" in run:
+    elif "old" in run and "b073" in run and "5" in run:
         plotting_index = 1
-    elif "new" in run and "b075" in run:
+    elif "new" in run and "b073" in run and "500" in run:
         plotting_index = 2
-    elif "old" in run and "b075" in run:
+    elif "old" in run and "b073" in run and "500" in run:
         plotting_index = 3
+    elif "new" in run and "b073" in run and "1000" in run:
+        plotting_index = 4
+    elif "old" in run and "b073" in run and "1000" in run:
+        plotting_index = 5
+    elif "new" in run and "b073" in run and "2000" in run:
+        plotting_index = 6
+    elif "old" in run and "b073" in run and "2000" in run:
+        plotting_index = 7
     else:
         plotting_index = None
 
     if plotting_index is not None:
         axs[plotting_index].scatter(filtered_planet['radius'] / 1000, filtered_planet['entropy'], s=2, label=run)
 
-axs[0].set_title("Stewart M-ANEOS, b = 0.73")
-axs[1].set_title("N-SPH M-ANEOS, b = 0.73")
-axs[2].set_title("Stewart M-ANEOS, b = 0.75")
-axs[3].set_title("N-SPH M-ANEOS, b = 0.75")
+axs[0].set_title(r"Stewart M-ANEOS, b = 0.73, $\rho_c=5 kg/m^3$")
+axs[1].set_title(r"N-SPH M-ANEOS, b = 0.73, $\rho_c=5 kg/m^3$")
+axs[2].set_title(r"Stewart M-ANEOS, b = 0.73, $\rho_c=500 kg/m^3$")
+axs[3].set_title(r"N-SPH M-ANEOS, b = 0.73, $\rho_c=500 kg/m^3$")
+axs[4].set_title(r"Stewart M-ANEOS, b = 0.73, $\rho_c=1000 kg/m^3$")
+axs[5].set_title(r"N-SPH M-ANEOS, b = 0.73, $\rho_c=1000 kg/m^3$")
+axs[6].set_title(r"Stewart M-ANEOS, b = 0.73, $\rho_c=2000 kg/m^3$")
+axs[7].set_title(r"N-SPH M-ANEOS, b = 0.73, $\rho_c=2000 kg/m^3$")
 
 for ax in axs:
     ax.set_xlabel("Radius (km)")
