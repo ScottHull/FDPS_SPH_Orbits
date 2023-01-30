9cfd5211f22e16dc759179eda35c577d2149053f
Scott Hull <ScottHull@users.noreply.github.com>
2023-01-30 11:53:46 -0500

    fixes to vapor.py interpolation

--- paper2_source_timeshots.py
+++ paper2_source_timeshots.py
@@ -137,4 +137,4 @@ for index, ax in enumerate(axs):
 axs[0].annotate("x ($10^4$ km)", xy=(0.0, -5.5), ha="center", fontsize=16, weight='bold')
 axs[0].annotate("y ($10^4$ km)", xy=(-5.5, 0.0), va="center", rotation=90, fontsize=16, weight='bold')
 
-plt.savefig("paper2_source_scenes.png", format='png', dpi=200)
+plt.savefig("paper2_source_scenes.png", format='png', dpi=300)
