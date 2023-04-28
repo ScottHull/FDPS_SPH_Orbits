f0753898727d85eea271c15919cc5bf2c4aa245e
Scott Hull <ScottHull@users.noreply.github.com>
2023-04-28 12:28:32 -0400

    working on fix_exploded_mars_target.py

--- fix_exploded_mars_target.py
+++ fix_exploded_mars_target.py
@@ -25,7 +25,7 @@ count = 0
 # place these particles in random locations within the planet
 for particle in df_ejected.index.tolist():
     count += 1
-    print(f"particle {count} of {num_particles_to_fix} (particle id: {df.at[particle, 'id']})")
+    print(f"particle {count} of {num_particles_to_fix} (particle id: {index})")
     # get random radius within planet
     radius = np.random.uniform(min(df_silicate['radius']), radius_planet - (800 * 1000))
     # get random theta
@@ -52,35 +52,47 @@ for particle in df_ejected.index.tolist():
         f = interp_functions[header]
         df.at[particle, header] = f(radius)
 
-# get all particles in df whose ids are in df_ejected
-df_fixed = df[df.index.isin(df_ejected.index.tolist())]
-# plot to check
-fig, axs = plt.subplots(2, 2, figsize=(10, 5))
-axs = axs.flatten()
-for index, header in enumerate(['density', 'internal energy', 'entropy', 'temperature']):
-    # scatter the particles and color by 'tag' column
-    axs[index].scatter(
-        df[df['tag'] % 2 == 0]['radius'] / 1000, df[df['tag'] % 2 == 0][header], s=2, alpha=0.5, color='red', label="Silicate Particles"
-    )
-    axs[index].scatter(
-        df[df['tag'] % 2 == 1]['radius'] / 1000, df[df['tag'] % 2 == 1][header], s=2, alpha=0.5, color='blue', label="Iron Particles"
-    )
-    axs[index].scatter(
-        df_fixed['radius'] / 1000, df_fixed[header], s=2, alpha=0.5, color='green', label="Fixed Particles"
-    )
-    # axs[index].scatter(
-    #     df['radius'] / 1000, df[header], s=2, alpha=0.5, label="All Particles"
-    # )
-    # plot the radius of the planet
-    axs[index].axvline(radius_planet / 1000, color='k', linestyle='--', label="Planet Radius")
-    # axs[index].set_title(header)
-    axs[index].set_ylabel(header)
-axs[0].legend(loc='upper right')
-for ax in axs:
-    ax.grid()
+# check for nan values in the dataframe and print out their ids if they exist
+nan_values = df[df.isna().any(axis=1)]
+if len(nan_values) > 0:
+    print("there are nan values in the dataframe")
+    print(nan_values.index.tolist())
+
+
+
+
+# # get all particles in df whose ids are in df_ejected
+# df_fixed = df[df.index.isin(df_ejected.index.tolist())]
+# # plot to check
+# fig, axs = plt.subplots(2, 2, figsize=(10, 5))
+# axs = axs.flatten()
+# for index, header in enumerate(['density', 'internal energy', 'entropy', 'temperature']):
+#     # scatter the particles and color by 'tag' column
+#     axs[index].scatter(
+#         df[df['tag'] % 2 == 0]['radius'] / 1000, df[df['tag'] % 2 == 0][header], s=2, alpha=0.5, color='red', label="Silicate Particles"
+#     )
+#     axs[index].scatter(
+#         df[df['tag'] % 2 == 1]['radius'] / 1000, df[df['tag'] % 2 == 1][header], s=2, alpha=0.5, color='blue', label="Iron Particles"
+#     )
+#     axs[index].scatter(
+#         df_fixed['radius'] / 1000, df_fixed[header], s=2, alpha=0.5, color='green', label="Fixed Particles"
+#     )
+#     # axs[index].scatter(
+#     #     df['radius'] / 1000, df[header], s=2, alpha=0.5, label="All Particles"
+#     # )
+#     # plot the radius of the planet
+#     axs[index].axvline(radius_planet / 1000, color='k', linestyle='--', label="Planet Radius")
+#     # axs[index].set_title(header)
+#     axs[index].set_ylabel(header)
+# axs[0].legend(loc='upper right')
+# for ax in axs:
+#     ax.grid()
+
+
+
 
 # remove the smoothing length column
 df = df.drop(columns=['smoothing length', 'radius'])
 # save the dataframe
 df.to_csv('body.dat', sep='\t', index=True, header=False)
-plt.savefig('test.png', format='png')
+# plt.savefig('test.png', format='png')
