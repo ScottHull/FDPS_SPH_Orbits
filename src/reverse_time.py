047b0b05ab0d23815f6b27118702978e840202e7
Scott Hull <ScottHull@users.noreply.github.com>
2023-06-12 19:36:51 -0400

    working on fix_exploded_mars_target.py

--- src/reverse_time.py
+++ src/reverse_time.py
@@ -15,8 +15,8 @@ class ReverseTime:
     def __init__(self, target_file_path, impactor_file_path, impact_parameter, dt=-0.1, center_target=False,
                  v_esc_multiple=1.0):
         self.G = 6.674 * 10 ** -11
-        self.target_df = pd.read_csv(target_file_path, sep='\t', skiprows=2, header=None)
-        self.impactor_df = pd.read_csv(impactor_file_path, sep='\t', skiprows=2, header=None)
+        self.target_df = pd.read_csv(target_file_path, sep='\t', skiprows=2, header=None).dropna()
+        self.impactor_df = pd.read_csv(impactor_file_path, sep='\t', skiprows=2, header=None).dropna()
         self.center_target = center_target
         self.target_mass = sum([float(i) for i in self.target_df[2]])
         self.impactor_mass = sum([float(i) for i in self.impactor_df[2]])
