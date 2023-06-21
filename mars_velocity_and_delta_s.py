f44128bae31517f4cd959b77f4cf20e19cb0230f
Scott Hull <ScottHull@users.noreply.github.com>
2023-06-21 02:09:59 -0400

    working on fix_exploded_mars_target.py

--- mars_velocity_and_delta_s.py
+++ mars_velocity_and_delta_s.py
@@ -26,6 +26,17 @@ increment = 5
 run_name = "500_mars"
 path = "/home/theia/scotthull/Paper2_SPH/gi/{}/{}".format(run_name, run_name)
 number_processes = 600
+min_normalize_parameter = 0
+max_normalize_parameter = 1000
+square_scale = 2e7
+path_3d = "3D_{}".format(run_name)
+if os.path.exists(path_3d):
+    shutil.rmtree(path_3d)
+os.mkdir(path_3d)
+
+plt.style.use("dark_background")
+normalizer = Normalize(min_normalize_parameter, max_normalize_parameter)
+cmap = cm.get_cmap('jet')
 
 iterations = []
 times = []
@@ -37,6 +48,10 @@ delta_s = []
 target_silicate_initial = None
 
 for index, iteration in enumerate(np.arange(start_iteration, end_iteration + increment, increment)):
+    fig = plt.figure(figsize=(10, 10))
+    fig.patch.set_facecolor('xkcd:black')
+    ax = fig.add_subplot(111, projection='3d')
+
     to_fname = "merged_{}_{}.dat".format(iteration, randint(0, 100000))
     cf = CombineFile(num_processes=number_processes, time=iteration, output_path=path, to_fname=to_fname)
     combined_file = cf.combine()
@@ -45,6 +60,42 @@ for index, iteration in enumerate(np.arange(start_iteration, end_iteration + inc
     target = df[df[1] <= 1]
     impactor = df[df[1] > 1]
     os.remove(to_fname)
+
+    ax.scatter(
+        target[3], target[4], target[5], s=5, c=target[13], cmap=cmap, norm=normalizer, alpha=1
+    )
+
+    ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    ax._axis3don = False
+    # ax.set_box_aspect(aspect=(1, 1, 1))
+    ax.set_xticks([])
+    # for minor ticks
+    ax.set_xticks([], minor=True)
+    ax.set_yticks([])
+    # for minor ticks
+    ax.set_yticks([], minor=True)
+    ax.set_zticks([])
+    # for minor ticks
+    ax.set_zticks([], minor=True)
+    ax.set_title(
+        str(formatted_time) + " hrs",
+        c="white",
+    )
+    sm = cm.ScalarMappable(norm=normalizer, cmap=cmap)
+    sm.set_array([])
+    cbaxes = inset_axes(ax, width="30%", height="3%", loc=2, borderpad=1.8)
+    cbar = plt.colorbar(sm, cax=cbaxes, orientation='horizontal')
+    cbar.ax.tick_params(labelsize=6)
+    cbar.ax.set_title(f"Delta Entropy", fontsize=6)
+    ax.set_title(f"{formatted_time} hrs")
+    ax.set_xlim(-square_scale, square_scale)
+    ax.set_ylim(-square_scale, square_scale)
+    ax.set_zlim(-square_scale, square_scale)
+    for i in get_cube_verts(square_scale=square_scale):
+        ax.plot(i[0], i[1], i[2], c='white', linewidth=0.3)
+    plt.savefig(f"{path_3d}/{iterations}.png".format(iteration), format='png', dpi=200)
 
     # get the mean velocity of the target and impactor
     iterations.append(iteration)
@@ -79,6 +130,15 @@ for index, iteration in enumerate(np.arange(start_iteration, end_iteration + inc
         ax.set_title("Delta S vs Radius")
         ax.grid()
         plt.savefig("delta_s_vs_radius.png", dpi=300)
+
+        animate(
+            start_time=start_iteration,
+            end_time=end_iteration,
+            interval=increment,
+            path=path_3d,
+            fps=10,
+            filename=f"{run_name}_impact_delta_s.mp4",
+        )
 
 fig = plt.figure(figsize=(10, 10))
 ax = fig.add_subplot(111)
