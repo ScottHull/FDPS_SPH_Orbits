2f6c23289f1774715be7e2fd1099c6e639183fc7
ScottHull <scottdhull21@gmail.com>
2023-07-27 01:07:28 -0400

    added animate_3D_simple.py

--- paper2_plot_phase_diagrams.py
+++ paper2_plot_phase_diagrams.py
@@ -134,7 +134,7 @@ axs[0].set_title("Canonical Impact")
 axs[1].set_title("Half-Earths Impact")
 plot_phase_diagrams()
 plt.tight_layout()
-legend = axs[-1].legend(fontsize=16)
+legend = axs[-1].legend(fontsize=14)
 for line in legend.get_lines():  # increase line widths in legend
     try:
         line.set_linewidth(4.0)
