2fbaddf11e3461540fc9332b6261d8debda4ce2b
ScottHull <scottdhull21@gmail.com>
2023-07-27 01:52:32 -0400

    added animate_3D_simple.py

--- paper1_plot_sawtooth_entropy.py
+++ paper1_plot_sawtooth_entropy.py
@@ -104,7 +104,7 @@ axs[-1].plot(
     [], [], linestyle="--", label="N-SPH M-ANEOS", color="black"
 )
 
-axs[0].set_ylim(upper=20)
+axs[0].set_ylim(top=20)
 
 axs[-1].legend(fontsize=14)
 plt.tight_layout()
