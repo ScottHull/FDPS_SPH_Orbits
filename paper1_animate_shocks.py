ada0165ca12341e2b7a5eb292f81d8ff3b003e2a
ScottHull <scottdhull21@gmail.com>
2023-08-10 23:52:50 -0400

    added animate_3D_simple.py

--- paper1_animate_shocks.py
+++ paper1_animate_shocks.py
@@ -8,6 +8,8 @@ from math import pi
 import string
 from random import randint
 import multiprocessing as mp
+from matplotlib.colors import Normalize
+import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 
 from src.animate import animate
@@ -18,6 +20,7 @@ angle = "b073"
 paths = [
     "5_b073_new", "500_b073_new",
 ]
+
 cutoff_densities = [5, 500]
 density_ranges = [[0, 20], [495, 520]]
 min_iteration = 0
@@ -25,6 +28,11 @@ max_iteration = 1800
 endstate_iteration = max_iteration
 increment = 1
 number_processes = 200
+norm_min = 5
+norm_max = 500
+
+normalizer = Normalize(norm_min, norm_max)
+cmap = cm.get_cmap("jet")
 
 plt.rcParams.update({'font.size': 16, })
 # plt.style.use("dark_background")
@@ -58,6 +66,9 @@ def get_endstate(s):
 disk_samples = {}
 
 for index, p in enumerate(paths):
+    if os.path.exists(f"shocks_{p}"):
+        shutil.rmtree(f"shocks_{p}")
+    os.mkdir(f"shocks_{p}")
     disk_samples.update({p: {
         'times': [],
         'samples': [],
@@ -69,6 +80,7 @@ for index, p in enumerate(paths):
     endstate_sample = endstate_sample.append(endstate[endstate['entropy'] < 9000].sample(10))
     cd = cutoff_densities[index]
     prev_df = None
+    prev_df_disk_sample = None
     for iteration in np.arange(min_iteration, max_iteration + increment, increment):
         path = base_path + "{}/{}".format(p, p)
         to_fname = "merged_{}_{}.dat".format(iteration, randint(0, 100000))
@@ -93,5 +105,66 @@ for index, p in enumerate(paths):
             #                                                   (disk_sample['z'] - row['z']) ** 2 <= row['smth'] ** 2])
         # calculate the change in entropy for each particle in the sample group
         if prev_df is not None:
+            disk['entropy_change'] = disk['entropy'] - prev_df['entropy']
             disk_sample['entropy_change'] = disk_sample['entropy'] - prev_df['entropy']
             disk_samples[p]['samples'].append(disk_sample)
+
+        fig, axs = plt.subplots(1, 4, figsize=(24, 6), sharex='all')
+        axs = axs.flatten()
+        for var, ylabel, ax in zip(['entropy', 'internal energy', 'temperature'],
+                                   [r'Entropy (J/kg/K)', r'Internal Energy (kJ)', r'Temperature (K)'],
+                                   axs[:-1]):
+            ax.grid()
+            ax.set_ylabel(ylabel)
+            ax.set_xlabel(r'Density (kg/m$^3$)')
+            ax.set_xlim(density_ranges[index][0], density_ranges[index][1])
+            ax.set_title(f"{formatted_time} hrs.")
+            ax.scatter(
+                disk['density'], disk[var], s=2, alpha=1, color=cmap(normalizer(disk['entropy change']))
+            )
+            ax.scatter(
+                disk_sample['density'], disk_sample[var], s=60, alpha=1
+            )
+        axs[-1].scatter(
+            disk_sample['density'], disk_sample['neighbors'], s=60, alpha=1
+        )
+        axs[-1].set_ylabel(r'Neighbors')
+        axs[-1].set_xlabel(r'Density (kg/m$^3$)')
+        axs[-1].set_xlim(density_ranges[index][0], density_ranges[index][1])
+        axs[-1].set_title(f"{formatted_time} hrs.")
+        axs[-1].grid()
+        plt.tight_layout()
+        plt.savefig(f"shocks_{p}/{iteration}.png")
+
+        prev_df = disk
+        prev_df_disk_sample = disk_sample
+
+    animate(
+        start_time=min_iteration,
+        end_time=max_iteration,
+        interval=increment,
+        path=f"shocks_{p}",
+        fps=80,
+        filename=f"{p}_shocks.mp4",
+    )
+
+
+for p in paths:
+    fig, axs = plt.subplots(1, 5, figsize=(30, 6), sharex='all')
+    axs = axs.flatten()
+    for ax in axs:
+        ax.grid()
+        ax.set_xlabel("Time (hours)", fontsize=16)
+    ylabels = [r'Density (kg/m$^3$)', r'Entropy (J/kg/K)', r'Internal Energy (kJ)', r'Temperature (K)', r'Neighbors']
+    for ax, y in zip(axs, ylabels):
+        ax.set_ylabel(y, fontsize=16)
+        ax.grid()
+        ax.set_xlim(0, 50)
+        ax.set_xlabel("Time (hrs.)", fontsize=16)
+        for particle in disk_samples[p]['samples']:
+            ax.plot(
+                disk_samples[p]['times'], particle[y], alpha=1
+            )
+    plt.tight_layout()
+    plt.savefig(f"{p}_shocks.png", dpi=300)
+
