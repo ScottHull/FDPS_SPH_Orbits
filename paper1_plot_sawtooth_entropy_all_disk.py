03015072d7a863e1855917b8590324d7842bd0ba
ScottHull <scottdhull21@gmail.com>
2023-08-19 09:34:08 -0400

    added animate_3D_simple.py

--- paper1_plot_sawtooth_entropy_all_disk.py
+++ paper1_plot_sawtooth_entropy_all_disk.py
@@ -83,7 +83,7 @@ for s, t in zip(names, titles):
     # get a list of the difference in entropy between the endstate and midstate
     select_particles = [i for i in endstate['id'].tolist() if endstate[endstate['id'] == i]['entropy'].values - midstate[midstate['id'] == i]['entropy'].values > 500]
     # get 5 random particles from the select_particles list
-    select_particles = np.random.choice(select_particles, 5)
+    select_particles = np.random.choice(select_particles, num_samples)
     # get the color cycle
     time = {i: [] for i in endstate['id'].values}
     entropy = {i: [] for i in endstate['id'].values}
@@ -164,7 +164,7 @@ for s, t in zip(names, titles):
         start_time=min_iteration,
         end_time=max_iteration,
         interval=increment,
-        path=to_path,
+        path=f"paper1_sawtooth_{s}",
         fps=10,
         filename=f"paper1_sawtooth_{s}"
     )
