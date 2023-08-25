686c9d270487b13ed529ccc38bdb2e0649f9c916
ScottHull <scottdhull21@gmail.com>
2023-08-25 17:10:02 -0400

    added animate_3D_simple.py

--- paper3_mars_mantle_melt_fraction.py
+++ paper3_mars_mantle_melt_fraction.py
@@ -29,9 +29,9 @@ iron_entropy = 1500
 headers = ["id", "tag", "mass", "x", "y", "z", "vx", "vy", "vz", "density", "internal energy", "pressure",
                    "potential energy", "entropy", "temperature"]
 
-initial_file = CombineFile(num_processes=num_processes, time=initial_iteration, output_path=f"{base_path}{paths[0][0]}/{paths[0][0]}")
-initial_df = initial_file.combine_df()
-initial_df.columns = headers
+# initial_file = CombineFile(num_processes=num_processes, time=initial_iteration, output_path=f"{base_path}{paths[0][0]}/{paths[0][0]}")
+# initial_df = initial_file.combine_df()
+# initial_df.columns = headers
 # change the index to the particle id
 initial_df.set_index('id', inplace=True)
 post_impact_file = CombineFile(num_processes=num_processes, time=post_impact_iteration, output_path=f"{base_path}{paths[0][0]}/{paths[0][0]}")
@@ -42,7 +42,7 @@ post_impact_df.set_index('id', inplace=True)
 
 # add a column called delta entropy
 # subtract sil_entropy if the tag column value is 1, else subtract iron_entropy
-initial_df["delta_entropy"] = initial_df.apply(lambda row: row["entropy"] - sil_entropy if row["tag"] % 2 == 0 else row["entropy"] - iron_entropy, axis=1)
+post_impact_df["delta_entropy"] = post_impact_df.apply(lambda row: row["entropy"] - sil_entropy if row["tag"] % 2 == 0 else row["entropy"] - iron_entropy, axis=1)
 
 for iteration in np.arange(50, 500 + 50, 50):
-    print(f"> {iteration}", len(initial_df["delta_entropy"]) / len(post_impact_df))
+    print(f"> {iteration}", len(post_impact_df["delta_entropy"]) / len(post_impact_df))
