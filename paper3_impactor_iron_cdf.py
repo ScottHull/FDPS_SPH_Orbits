103de4e20f5ea6ed494591be7fdfbecb0791ad42
ScottHull <scottdhull21@gmail.com>
2023-09-12 11:11:21 -0400

    added animate_3D_simple.py

--- paper3_impactor_iron_cdf.py
+++ paper3_impactor_iron_cdf.py
@@ -38,6 +38,8 @@ df.columns = headers
 df['radius'] = np.sqrt(df['x'] ** 2 + df['y'] ** 2 + df['z'] ** 2) / 1000
 particles_within_mars = df[df['radius'] <= mars_radius]
 impactor_iron_within_mars = particles_within_mars[particles_within_mars['tag'] == 3]
+# sort particles by radius
+impactor_iron_within_mars = impactor_iron_within_mars.sort_values(by=['radius'])
 # create a column of the cumulative sum of the mass of the particles within each radius
 impactor_iron_within_mars['cumulative_mass'] = impactor_iron_within_mars['mass'].cumsum() / impactor_iron_within_mars['mass'].sum()
 
