333b60dce40a7765f903f442825271259c3698ed
Scott Hull <ScottHull@users.noreply.github.com>
2023-12-20 15:10:58 -0500

    added paper3_hydrodynamic_initial_conditions.py

--- paper2_investigate_hydrodynamic_initial_conditions.py
+++ paper2_investigate_hydrodynamic_initial_conditions.py
@@ -7,6 +7,9 @@ from statistics import mean
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize, LogNorm
+import matplotlib.cm as cm
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 from src.combine import CombineFile
 from src.animate import animate
@@ -303,3 +306,119 @@ for ax in axs[8:10]:
 # make tight layout with no hspace
 plt.tight_layout()
 plt.savefig("paper2_initial_condition_thermodynamics.png", format='png', dpi=200)
+
+
+# make a 2 row 3 column figure
+fig, axs = plt.subplots(2, 3, figsize=(15, 10), sharex='all', sharey='all')
+axs = axs.flatten()
+
+temperature_normalizer = Normalize(1800, 6000)
+entropy_normalizer = Normalize(2000, 6000)
+vmf_normalizer = Normalize(0, 100)
+cmap = cm.get_cmap('cool')
+
+plot_index = 0
+for index, (run, verbose_run_name, iteration) in enumerate(runs):
+    # if len(run) == 0, then skip this part of the loop
+    if len(run) == 0:
+        continue
+    run_name = run.split('/')[-1]
+    base_path = run.split(f"/{run_name}")[0] + "/"
+    run = run_name
+    if not os.path.exists(f"{run}_vel_profile"):
+        os.mkdir(f"{run}_vel_profile")
+    circ_path = base_path + f"/{run}/circularized_{run}"
+    end_time_df = pd.read_csv(
+        os.path.join(circ_path, f"{max_iteration}.csv"),
+    )
+    end_time_disk = end_time_df[end_time_df["label"] == "DISK"]
+    end_time_particle_ids = end_time_disk["id"].values
+    time, iterations, mean_disk_vel, mean_disk_entropy, mean_disk_temperature, final_disk_vmf, all_silicate_vmf = [], [], [], [], [], [], []
+    phase_path = new_phase_path if "new" in run else old_phase_path
+
+    axs[index].set_title(verbose_run_name, fontsize=20)
+
+    path = base_path + f"{run}/{run}"
+    to_fname = "merged_{}_{}.dat".format(iteration, randint(0, 100000))
+    cf = CombineFile(num_processes=number_processes, time=iteration, output_path=path, to_fname=to_fname)
+    c = cf.combine()
+    formatted_time = round(cf.sim_time * 0.000277778, 2)
+
+    combined_file = pd.read_csv(to_fname, skiprows=2, header=None, delimiter="\t")
+
+    os.remove(to_fname)
+
+    final_disk_particles = combined_file[combined_file[0].isin(end_time_particle_ids)]
+
+    id, x, y, z, vx, vy, vz, entropy, temperature = combined_file[0], combined_file[3], combined_file[4], \
+        combined_file[5], combined_file[6], combined_file[7], combined_file[8], combined_file[13], combined_file[14]
+
+    id_disk, x_disk, y_disk, z_disk, vx_disk, vy_disk, vz_disk, entropy_disk, temperature_disk = \
+        final_disk_particles[0], final_disk_particles[3], final_disk_particles[4], final_disk_particles[5], \
+            final_disk_particles[6], final_disk_particles[7], final_disk_particles[8], final_disk_particles[13], \
+            final_disk_particles[14]
+
+    velocity = np.sqrt(vx_disk ** 2 + vy_disk ** 2 + vz_disk ** 2)
+
+    time.append(formatted_time)
+    mean_disk_vel.append(velocity.mean())
+    mean_disk_entropy.append(entropy_disk.mean())
+    mean_disk_temperature.append(temperature_disk.mean())
+
+    # replace final_disk_particles headers with the correct headers
+    final_disk_particles.columns = [
+        'id', 'tag', 'mass', 'x', 'y', 'z', 'vx', 'vy', 'vz', 'density', 'internal_energy', 'pressure',
+        'potential_energy', 'entropy', 'temperature'
+    ]
+    combined_file.columns = final_disk_particles.columns
+    final_disk_particles['velocity'] = np.sqrt(final_disk_particles['vx'] ** 2 + final_disk_particles['vy'] ** 2 + final_disk_particles['vz'] ** 2)
+    df2 = final_disk_particles[final_disk_particles['tag'] % 2 == 0]
+
+    vmf_final_disk = calc_vapor_mass_fraction_without_circularization_from_formatted(
+        df2, phase_path, restrict_df=False
+    ) * 100
+
+    axs[plot_index].scatter(
+        combined_file['x'] / 1000, combined_file['y'] / 1000, s=5, marker=".", color='black'
+    )
+    axs[plot_index].scatter(
+        df2['x'] / 1000, df2['y'] / 1000, s=5, marker=".", color=cmap(temperature_normalizer(df2['temperature']))
+    )
+    axs[plot_index].text(
+        0.85, 0.90, f"{run_name}", transform=axs[plot_index].transAxes, verticalalignment='top',
+        fontsize=18, fontweight='bold'
+    )
+    plot_index += 1
+    axs[plot_index].scatter(
+        combined_file['x'] / 1000, combined_file['y'] / 1000, s=5, marker=".", color='black'
+    )
+    axs[plot_index].scatter(
+        combined_file['x'] / 1000, combined_file['y'] / 1000, s=5, marker=".", color='black'
+    )
+    axs[plot_index].scatter(
+        df2['x'] / 1000, df2['y'] / 1000, s=5, marker=".", color=cmap(temperature_normalizer(df2['entropy']))
+    )
+    plot_index += 1
+    axs[plot_index].scatter(
+        combined_file['x'] / 1000, combined_file['y'] / 1000, s=5, marker=".", color='black'
+    )
+    axs[plot_index].scatter(
+        df2['x'] / 1000, df2['y'] / 1000, s=5, marker=".", color=cmap(vmf_normalizer(df2['vmf_wo_circ'] * 100))
+    )
+    plot_index += 1
+
+for ax in [axs[0], axs[3]]:
+    ax.set_ylabel("y (km)", fontsize=16)
+for ax in axs[-3:]:
+    ax.set_xlabel("x (km)", fontsize=16)
+for ax, label, normalizer in zip(axs[:3], ['Temperature (K)', 'Entropy (J/kg/K)', 'VMF (%)'],
+                                 [temperature_normalizer, entropy_normalizer, vmf_normalizer]):
+    sm = cm.ScalarMappable(norm=normalizer, cmap=cmap)
+    sm.set_array([])
+    cbaxes = inset_axes(axs[0], width="70%", height="10%", loc=1, borderpad=1.8)
+    cbar = plt.colorbar(sm, cax=cbaxes, orientation='horizontal')
+    cbar.ax.tick_params(labelsize=14)
+    cbar.ax.set_title(label, fontsize=16)
+
+plt.tight_layout()
+plt.savefig("paper2_initial_condition_thermodynamics_as_func_space.png", format='png', dpi=200)
